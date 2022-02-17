c1c8f3583535fb4a6f129991d740dfc10284b624
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2022-02-17 18:21:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+// burger menu
 var burger = document.getElementById("burger-menu");
 var nav = document.getElementById("header-nav");
 burger.onclick = function() {
+   // отображение нав-меню
     if(nav.style.display == "flex") {
         nav.style.display = 'none';
      } else {
         nav.style.display = 'flex';
      };
+   //   скрытие welcome content
      if(welcome__content.style.display == "none") {
         welcome__content.style.display = 'flex';
      } else {
@@ -13,3 +16,16 @@ burger.onclick = function() {
      };
 
 };
+
+// booking window
+var closeBtn = document.getElementById("booking__close-btn");
+var bookingWindow = document.getElementById("booking-window");
+var buyBtn = document.getElementById("tickets__buy-btn");
+closeBtn.onclick = function() {
+   // закрытие окна
+   bookingWindow.style.left = '-100%';
+}
+buyBtn.onclick = function() {
+   // открытие окна
+   bookingWindow.style.left = '0';
+}
