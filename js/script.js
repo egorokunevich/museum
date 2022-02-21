4d27fdff83006511e2837920ac8130267fce5d80
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2022-02-21 23:46:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,11 @@
-const screenWidth = window.screen.width;
-console.log("screen width: ", screenWidth);
-// burger menu
+// Переменная для получения ширины окна пользователя: window.screen.width
+
+// Переменные для бургер-меню
 const burger = document.getElementById("burger-menu");
 const nav = document.getElementById("header-nav");
 const welcomeContent = document.getElementById("welcome__content");
 
-// burger.onclick = function () {
-//   if (window.screen.width > 1024) {
-//     nav.classList.toggle("nav_active");
-//     console.log(">1024px: toggle nav");
-//   } else if (window.screen.width <= 768) {
-//     nav.classList.toggle("nav_active");
-//     console.log("<=768px: toggle nav");
-//   } else {
-//     nav.classList.toggle("nav_active");
-//     welcomeContent.classList.toggle("welcome-content_inactive");
-//     console.log("768px < .. <= 1024px: toggle nav, toggle content");
-//   }
-// };
-
+// Переключение видимости навигации при клике на бургер-меню.
 burger.onclick = function () {
   if (window.screen.width > 1024) {
     if (nav.classList.contains("nav_active")) {
@@ -39,6 +26,7 @@ burger.onclick = function () {
       nav.classList.add("nav_active");
     }
     
+    // Переключение видимости контента welcome.
     if (welcomeContent.classList.contains("welcome-content_active")) {
       welcomeContent.classList.remove("welcome-content_active");
     } else {
@@ -46,29 +34,19 @@ burger.onclick = function () {
     }
   }
 
+  // Переключение иконки бургер-меню.
   burger.classList.toggle("burger_active");
 };
 
-// if (nav.classList.contains("nav_active")) {
-//   nav.classList.remove("nav_active");
-// } else {
-//   nav.classList.add("nav_active");
-// }
-// if (welcomeContent.classList.contains("welcome-content_inactive")) {
-//   welcomeContent.classList.remove("welcome-content_inactive");
-// } else {
-//   welcomeContent.classList.add("welcome-content_inactive");
-// }
-
-// booking form
+// Переменные для окна Booking
 const closeBtn = document.getElementById("booking__close-btn");
 const bookingWindow = document.getElementById("booking-window");
 const buyBtn = document.getElementById("tickets__buy-btn");
 closeBtn.onclick = function () {
-  // закрытие окна
+  // Закрытие окна по кнопке "Close"
   bookingWindow.classList.remove("booking_active");
 };
 buyBtn.onclick = function () {
-  // открытие окна
+  // Открытие окна по кнопке "Buy now"
   bookingWindow.classList.add("booking_active");
 };
