525604d0230429e29b9a3483343f5d79e7cd5466
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2022-03-04 13:41:44 +0300

    Nav-menu update for mobile devices

---
+++ js/booking.js
@@ -0,0 +1,170 @@
+"use strict";
+
+// The tickets' amount in "Tickets" section
+const ticketsSeniorAmount = document.getElementById("ticketsSeniorAmount"); // Tickets: input=[number] Senior
+const ticketsBasicAmount = document.getElementById("ticketsBasicAmount"); // Tickets: input=[number] Basic
+// The tickets' amount in "Booking" form
+const bookingSeniorAmount = document.getElementById("booking_seniorAmount"); // Booking: input=[number] Senior
+const bookingBasicAmount = document.getElementById("booking_basicAmount"); // Booking: input=[number] Basic
+// The tickets' amount in "Overview"
+const overviewSeniorAmount = document.getElementById("overview_seniorAmount"); // Overview: senior amount
+const overviewBasicAmount = document.getElementById("overview_basicAmount"); // Overview: basic amount
+// Each ticket's price in "Overview"
+const basicPrice = document.getElementById("basicPrice"); // Basic's Price
+const seniorPrice = document.getElementById("seniorPrice"); // Senior's price
+// Total price in "Overview"
+const overviewTotal = document.getElementById("overviewTotal");
+// Total price in "Tickets" section
+const ticketsTotal = document.getElementById("ticketsTotal");
+// Variables used to call a "Booking" form
+const closeBtn = document.getElementById("booking__close-btn"); // "Close" btn
+const bookingWindow = document.getElementById("booking-window"); // "Booking" form
+const buyBtn = document.getElementById("tickets__buy-btn"); // "BuyNow" btn
+closeBtn.onclick = function () {
+  // Closes "Booking" form when "Close" btn is clicked
+  bookingWindow.classList.remove("booking_active");
+  ticketsBasicAmount.value = bookingBasicAmount.value;
+  ticketsSeniorAmount.value = bookingSeniorAmount.value;
+  ticketsTotal.innerHTML = "Total: " + overviewTotal.innerHTML;
+};
+buyBtn.onclick = function () {
+  // Sends tickets' amount from "Tickets" to "Booking" form when form is called
+  bookingSeniorAmount.value = ticketsSeniorAmount.value;
+  bookingBasicAmount.value = ticketsBasicAmount.value;
+
+  // Sends tickets' amount to "Overview" when form is called
+  overviewSeniorAmount.innerHTML = bookingSeniorAmount.value;
+  overviewBasicAmount.innerHTML = bookingBasicAmount.value;
+
+  // Calculates ticket's price of each type when form is called
+  basicPrice.innerHTML = bookingBasicAmount.value * 20 + " €";
+  seniorPrice.innerHTML = bookingSeniorAmount.value * 10 + " €";
+  overviewTotal.innerHTML =
+    bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
+
+  // Calls "Booking" form when "Buy now" btn is clicked
+  bookingWindow.classList.add("booking_active");
+};
+
+
+// Variables for Steppers in form
+const bookingBasicAdd = document.getElementById("booking_basicAdd");
+const bookingBasicRemove = document.getElementById("booking_basicRemove");
+const bookingSeniorAdd = document.getElementById("booking_seniorAdd");
+const bookingSeniorRemove = document.getElementById("booking_seniorRemove");
+
+// Variables for Steppers in "Tickets" section
+const ticketsBasicAdd = document.getElementById("ticketsBasicAdd");
+const ticketsBasicRemove = document.getElementById("ticketsBasicRemove");
+const ticketsSeniorAdd = document.getElementById("ticketsSeniorAdd");
+const ticketsSeniorRemove = document.getElementById("ticketsSeniorRemove");
+
+// Variables with ticket's price of each type
+const price20 = 20; // for Basic type
+const price10 = 10; // for Senior type
+
+// "+" btn in "Booking" form
+function bookingAdd(add, amount, price, overviewAmount, priceVar) {
+  add.onclick = function () {
+    amount.value++;
+    overviewAmount.innerHTML = amount.value;
+    price.innerHTML = amount.value * priceVar + " €";
+    overviewTotal.innerHTML =
+      bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
+  };
+}
+
+// "-" btn in "Booking" form
+function bookingRemove(remove, amount, price, overviewAmount, priceVar) {
+  remove.onclick = function () {
+    if (amount.value > 0) {
+      amount.value--;
+      overviewAmount.innerHTML = amount.value;
+      price.innerHTML = amount.value * priceVar + " €";
+      overviewTotal.innerHTML =
+        bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
+    } else {
+      amount.value = 0;
+      overviewAmount.innerHTML = amount.value;
+      price.innerHTML = amount.value * priceVar + " €";
+      overviewTotal.innerHTML =
+        bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
+    }
+  };
+}
+
+// Booking Basic Stepper
+bookingAdd(
+  bookingBasicAdd,
+  bookingBasicAmount,
+  basicPrice,
+  overviewBasicAmount,
+  price20
+);
+bookingRemove(
+  bookingBasicRemove,
+  bookingBasicAmount,
+  basicPrice,
+  overviewBasicAmount,
+  price20
+);
+
+// Booking Senior Stepper
+bookingAdd(
+  bookingSeniorAdd,
+  bookingSeniorAmount,
+  seniorPrice,
+  overviewSeniorAmount,
+  price10
+);
+bookingRemove(
+  bookingSeniorRemove,
+  bookingSeniorAmount,
+  seniorPrice,
+  overviewSeniorAmount,
+  price10
+);
+
+// "+" btn in "Tickets" section
+function ticketsAdd(add, amount) {
+  add.onclick = function () {
+    amount.value++;
+    ticketsTotal.innerHTML =
+      "Total: " +
+      (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
+      " €";
+  };
+}
+
+// "-" btn in "Tickets" section
+function ticketsRemove(remove, amount) {
+  remove.onclick = function () {
+    if (amount.value > 0) {
+      amount.value--;
+      ticketsTotal.innerHTML =
+        "Total: " +
+        (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
+        " €";
+    } else {
+      amount.value = 0;
+      ticketsTotal.innerHTML =
+        "Total: " +
+        (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
+        " €";
+    }
+  };
+}
+
+// Tickets Steppers
+ticketsAdd(ticketsBasicAdd, ticketsBasicAmount);
+ticketsRemove(ticketsBasicRemove, ticketsBasicAmount);
+ticketsAdd(ticketsSeniorAdd, ticketsSeniorAmount);
+ticketsRemove(ticketsSeniorRemove, ticketsSeniorAmount);
+
+// Refreshing a Total in "Tickets" section on a page load
+window.onload = (event) => {
+  ticketsTotal.innerHTML =
+    "Total: " +
+    (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
+    " €";
+};
--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-// Переменные для бургер-меню
+"use strict";
+
+//--------------------
+// --- Burger-menu ---
+//--------------------
+
+// Burger-menu variables
 const burger = document.getElementById("burger-menu");
 const nav = document.getElementById("header-nav");
 const welcomeContent = document.getElementById("welcome__content");
 
-// Переключение видимости навигации при клике на бургер-меню.
+// Toggling a nav's visibility when burger-menu is clicked.
 burger.onclick = function () {
   if (window.screen.width > 1024) {
     if (nav.classList.contains("nav_active")) {
@@ -25,7 +31,7 @@ burger.onclick = function () {
     }
   }
 
-  // Переключение иконки бургер-меню.
+  // Burger-menu's icon toggle.
   burger.classList.toggle("burger_active");
 };
 document.onclick = function () {
@@ -40,195 +46,45 @@ document.onclick = function () {
   }
 };
 
-// Закрывает бургер-меню при выборе одного из элементов
-document.querySelector(".nav-link").onclick = function () {
-  if (nav.classList.contains("nav_active")) {
-    nav.classList.remove("nav_active");
-  }
-};
-
-// Кол-во билетов в секции Tickets
-const ticketsSeniorAmount = document.getElementById("ticketsSeniorAmount"); // Tickets: input=[number] Senior
-const ticketsBasicAmount = document.getElementById("ticketsBasicAmount"); // Tickets: input=[number] Basic
-// Кол-во билетов в форме
-const bookingSeniorAmount = document.getElementById("booking_seniorAmount"); // Booking: input=[number] Senior
-const bookingBasicAmount = document.getElementById("booking_basicAmount"); // Booking: input=[number] Basic
-// Кол-во билетов в Overview
-const overviewSeniorAmount = document.getElementById("overview_seniorAmount"); // Overview: senior amount
-const overviewBasicAmount = document.getElementById("overview_basicAmount"); // Overview: basic amount
-// Стоимость каждого типа билетов
-const basicPrice = document.getElementById("basicPrice"); // Цена
-const seniorPrice = document.getElementById("seniorPrice"); // Цена
-// Общая стоимость в Overview
-const overviewTotal = document.getElementById("overviewTotal");
-// Общая стоимость в секции Tickets
-const ticketsTotal = document.getElementById("ticketsTotal");
-// Переменные для вызова формы Booking
-const closeBtn = document.getElementById("booking__close-btn"); // Кнопка закрытия
-const bookingWindow = document.getElementById("booking-window"); // Форма Booking
-const buyBtn = document.getElementById("tickets__buy-btn"); // Кнопка BuyNow
-closeBtn.onclick = function () {
-  // Закрывает окно по кнопке "Close"
-  bookingWindow.classList.remove("booking_active");
-  ticketsBasicAmount.value = bookingBasicAmount.value;
-  ticketsSeniorAmount.value = bookingSeniorAmount.value;
-  ticketsTotal.innerHTML = "Total: " + overviewTotal.innerHTML;
-};
-buyBtn.onclick = function () {
-  // Передаёт количество билетов из секции Tickets в форму Booking при вызове формы
-  bookingSeniorAmount.value = ticketsSeniorAmount.value;
-  bookingBasicAmount.value = ticketsBasicAmount.value;
-
-  // Передаёт количество билетов в Overview при вызове формы
-  overviewSeniorAmount.innerHTML = bookingSeniorAmount.value;
-  overviewBasicAmount.innerHTML = bookingBasicAmount.value;
-
-  // Считает стоимость каждого типа билета при вызове формы
-  basicPrice.innerHTML = bookingBasicAmount.value * 20 + " €";
-  seniorPrice.innerHTML = bookingSeniorAmount.value * 10 + " €";
-  overviewTotal.innerHTML =
-    bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
-
-  // Открывает окно по кнопке "Buy now"
-  bookingWindow.classList.add("booking_active");
-};
-
-const bookingBasicAdd = document.getElementById("booking_basicAdd");
-const bookingBasicRemove = document.getElementById("booking_basicRemove");
-const bookingSeniorAdd = document.getElementById("booking_seniorAdd");
-const bookingSeniorRemove = document.getElementById("booking_seniorRemove");
-
-const ticketsBasicAdd = document.getElementById("ticketsBasicAdd");
-const ticketsBasicRemove = document.getElementById("ticketsBasicRemove");
-const ticketsSeniorAdd = document.getElementById("ticketsSeniorAdd");
-const ticketsSeniorRemove = document.getElementById("ticketsSeniorRemove");
-
-// Переменная, содержащая цену за билет
-const price20 = 20;
-const price10 = 10;
-
-// Кнопка "+" в booking
-function bookingAdd(add, amount, price, overviewAmount, priceVar) {
-  add.onclick = function () {
-    amount.value++;
-    overviewAmount.innerHTML = amount.value;
-    price.innerHTML = amount.value * priceVar + " €";
-    overviewTotal.innerHTML =
-      bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
-  };
+// Closing a nav-menu if an option was clicked
+let navList = document.querySelectorAll(".nav-link");
+for (let i = 0; i < navList.length; i++) {
+  // Adding an eventListener on every element on the list
+  navList[i].addEventListener("click", () => {
+    closeNav(); // Calling a function if an element is clicked
+    burgerToggle(); // Calling a function if an element is clicked
+  });
+}
+function closeNav() {
+  // This function closes nav-menu
+  nav.classList.remove("nav_active");
+}
+function burgerToggle() {
+  // This function changes burger-menu's icon to default
+  burger.classList.toggle("burger_active");
 }
 
-// Кнопка "-" в booking
-function bookingRemove(remove, amount, price, overviewAmount, priceVar) {
-  remove.onclick = function () {
-    if (amount.value > 0) {
-      amount.value--;
-      overviewAmount.innerHTML = amount.value;
-      price.innerHTML = amount.value * priceVar + " €";
-      overviewTotal.innerHTML =
-        bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
-    } else {
-      amount.value = 0;
-      overviewAmount.innerHTML = amount.value;
-      price.innerHTML = amount.value * priceVar + " €";
-      overviewTotal.innerHTML =
-        bookingBasicAmount.value * 20 + bookingSeniorAmount.value * 10 + " €";
-    }
-  };
-}
+//---------------------
+// -- Explore-slider --
+//---------------------
 
-// Booking Basic Stepper
-bookingAdd(
-  bookingBasicAdd,
-  bookingBasicAmount,
-  basicPrice,
-  overviewBasicAmount,
-  price20
-);
-bookingRemove(
-  bookingBasicRemove,
-  bookingBasicAmount,
-  basicPrice,
-  overviewBasicAmount,
-  price20
-);
-
-// Booking Senior Stepper
-bookingAdd(
-  bookingSeniorAdd,
-  bookingSeniorAmount,
-  seniorPrice,
-  overviewSeniorAmount,
-  price10
-);
-bookingRemove(
-  bookingSeniorRemove,
-  bookingSeniorAmount,
-  seniorPrice,
-  overviewSeniorAmount,
-  price10
-);
-
-// Кнопка "+" в Tickets
-function ticketsAdd(add, amount) {
-  add.onclick = function () {
-    amount.value++;
-    ticketsTotal.innerHTML =
-      "Total: " +
-      (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
-      " €";
-  };
-}
-
-// Кнопка "-" в Tickets
-function ticketsRemove(remove, amount) {
-  remove.onclick = function () {
-    if (amount.value > 0) {
-      amount.value--;
-      ticketsTotal.innerHTML =
-        "Total: " +
-        (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
-        " €";
-    } else {
-      amount.value = 0;
-      ticketsTotal.innerHTML =
-        "Total: " +
-        (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
-        " €";
-    }
-  };
-}
-
-// Tickets Steppers
-ticketsAdd(ticketsBasicAdd, ticketsBasicAmount);
-ticketsRemove(ticketsBasicRemove, ticketsBasicAmount);
-ticketsAdd(ticketsSeniorAdd, ticketsSeniorAmount);
-ticketsRemove(ticketsSeniorRemove, ticketsSeniorAmount);
-
-// Обновляет сумму Total в Tickets при обновлении страницы
-window.onload = (event) => {
-  ticketsTotal.innerHTML =
-    "Total: " +
-    (ticketsBasicAmount.value * 20 + ticketsSeniorAmount.value * 10) +
-    " €";
-};
-
-//explore slider
-
-let sliderControl = document.querySelector("div.explore-slider__control");
-let sliderAfter = document.querySelector("div.after-container");
+let sliderControl = document.querySelector("div.explore-slider__control"); // Custom thumb for a slider
+let sliderAfter = document.querySelector("div.after-container"); // Container with "after" img
 let rangeThumb = document.querySelector(
   "div.explore-slider input[name=exploreRange]"
-);
+); // Range's thumb
 
 rangeThumb.oninput = function () {
   sliderControl.style.left = rangeThumb.value + "%";
+
+  // This conditional statement(if) sets an "after" container's width depending on range's value
   if (rangeThumb.value == "99") {
     sliderAfter.style.width = 0 + "%";
   } else {
     sliderAfter.style.width = 100 - rangeThumb.value + "%";
   }
 
-  console.log("value: " + rangeThumb.value);
-  console.log("width: " + sliderAfter.style.width);
+  // Used to check values in console
+  // console.log("value: " + rangeThumb.value);
+  // console.log("width: " + sliderAfter.style.width);
 };
